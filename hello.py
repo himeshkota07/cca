d42bf0713bded06568160ee8fc3f19d2bb4a3f42
Himesh Kota <154901349+himeshkota07@users.noreply.github.com>
2025-03-12 09:59:58 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,11 +2,15 @@ import random
 print("Welcome to the guessing game")
 n=random.randint(100)
 win=False
-for i in range(5):
+attempts=0
+while(attempts<=5):
   guess=int(input("Guess the number between 0 to 100: "))
+  attempts+=1
   if(n==guess):
     print("Correct guess.You won!")
     win=True
-    return 0
   else:
     print("Try again😊!")
+
+if(win==False):
+  print("You lost the game. Better luck next time")
