2c0c5ac78c6e2070a2155acf8005b8f65ca6f51e
Himesh Kota <154901349+himeshkota07@users.noreply.github.com>
2025-03-18 22:06:26 +0530

    Update bunking.py

--- bunking.py
+++ bunking.py
@@ -4,18 +4,31 @@ n=int(input("Enter total no.of subjects: "))
 limit=int(input("Enter the minimum required attendance(in %): "))
 subjects=[]
 Classes={}
+Current={}
+
 for i in range(1,n+1):
     sub=input(f"Enter subject {i}: ")
     subjects.append(sub)
+
+
 for i in range(1,n+1):
     sub=subjects[i-1]
     hours=int(input(f"Enter total no.of hours for subject {subjects[i-1]}: "))
     Classes[sub]=hours
+
+
+for i in range(1,n+1):
+    sub=subjects[i-1]
+    hours=int(input(f"Enter no.of hours completed for subject {subjects[i-1]}: "))
+    Current[sub]=hours
+
 attendance={}
+
+
 for i in range(1,n+1):
     present=int(input(f"Enter the number of classes attended for subject {subjects[i-1]}: "))
     sub=subjects[i-1]
-    attendance[sub]=math.floor((present/Classes[sub])*100)
+    attendance[sub]=present
 print("These are the subjects present: ")
 print(subjects)
 user_sub=input("Enter the subject you need assistance in bunking: ")
@@ -23,12 +36,14 @@ user_sub=input("Enter the subject you need assistance in bunking: ")
 
 def bunking(course):
     current_attendance=attendance.get(course)
-    bunk=limit-current_attendance
-    if(limit<=0):
-        print("Sorry!You cannot bunk any more classes in this subject.")
+    bunk=Current.get(course)-current_attendance
+    total_bunk=math.floor((100-limit)*Classes.get(course))
+    res=total_bunk-bunk
+    if(res>0):
+        print(f"You can bunk {res} more hours in this subject!")
     else:
-        res=math.floor((bunk/100)*Classes.get(course))
-        print(f"You can bunk {res} more classes in {user_sub} this semester")
+        print("Sorry! You cannot bunk any more for this subject")
+
 
 
 bunking(user_sub)
